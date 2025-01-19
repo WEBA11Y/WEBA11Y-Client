29c1a63a4d2823bbfe16758b727ac607a6b54812
juhee067 <66045537+juhee067@users.noreply.github.com>
2025-01-19 20:09:59 +0900

    style: 타이포그래피 구조 수정 #1

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -55,97 +55,112 @@ const typo = {
   title: {
     lg: {
       fontSize: "48px",
-      fontWeight: "600", // semibold
+      fontWeight: 600,
       lineHeight: "56px",
     },
     md: {
-      bold: {
-        fontSize: "36px",
-        fontWeight: "600", // semibold
-        lineHeight: "44px",
-      },
-      medium: {
-        fontSize: "36px",
-        fontWeight: "500", // medium
-        lineHeight: "44px",
-      },
-      regular: {
-        fontSize: "36px",
-        fontWeight: "400", // regular
-        lineHeight: "44px",
-      },
+      fontSize: "36px",
+      fontWeight: 600,
+      lineHeight: "44px",
     },
     sm: {
       fontSize: "24px",
-      fontWeight: "600", // semibold
+      fontWeight: 600,
       lineHeight: "32px",
     },
     xs: {
       fontSize: "20px",
-      fontWeight: "600", // semibold
+      fontWeight: 600,
       lineHeight: "28px",
     },
   },
   text: {
     lg: {
       fontSize: "18px",
-      fontWeight: "600", // semibold
+      fontWeight: 600,
       lineHeight: "28px",
     },
     md: {
-      bold: {
-        fontSize: "16px",
-        fontWeight: "600", // semibold
-        lineHeight: "24px",
-      },
-      medium: {
-        fontSize: "16px",
-        fontWeight: "500", // medium
-        lineHeight: "24px",
-      },
-      regular: {
-        fontSize: "16px",
-        fontWeight: "400", // regular
-        lineHeight: "24px",
-      },
+      fontSize: "16px",
+      fontWeight: 400,
+      lineHeight: "24px",
     },
     sm: {
       fontSize: "14px",
-      fontWeight: "400", // regular
+      fontWeight: 400,
       lineHeight: "20px",
     },
     xs: {
       fontSize: "12px",
-      fontWeight: "400", // regular
+      fontWeight: 400,
       lineHeight: "16px",
     },
   },
   button: {
     lg: {
       fontSize: "16px",
-      fontWeight: "700", // bold
+      fontWeight: 700,
       lineHeight: "20px",
     },
     md: {
       fontSize: "14px",
-      fontWeight: "600", // semibold
-      lineHeight: "14px",
+      fontWeight: 600,
+      lineHeight: "18px",
     },
     sm: {
       fontSize: "12px",
-      fontWeight: "600", // semibold
-      lineHeight: "12px",
+      fontWeight: 600,
+      lineHeight: "16px",
+    },
+    xs: {
+      fontSize: "10px",
+      fontWeight: 600,
+      lineHeight: "14px",
     },
   },
   caption: {
-    fontSize: "12px",
-    fontWeight: "400", // regular
-    lineHeight: "16px",
+    lg: {
+      fontSize: "14px",
+      fontWeight: 400,
+      lineHeight: "20px",
+    },
+    md: {
+      fontSize: "12px",
+      fontWeight: 400,
+      lineHeight: "16px",
+    },
+    sm: {
+      fontSize: "10px",
+      fontWeight: 400,
+      lineHeight: "14px",
+    },
+    xs: {
+      fontSize: "8px",
+      fontWeight: 400,
+      lineHeight: "12px",
+    },
   },
   overline: {
-    fontSize: "10px",
-    fontWeight: "700", // bold
-    lineHeight: "10px",
+    lg: {
+      fontSize: "12px",
+      fontWeight: 700,
+      lineHeight: "16px",
+    },
+    md: {
+      fontSize: "10px",
+      fontWeight: 700,
+      lineHeight: "14px",
+    },
+    sm: {
+      fontSize: "8px",
+      fontWeight: 700,
+      lineHeight: "12px",
+    },
+    xs: {
+      fontSize: "6px",
+      fontWeight: 700,
+      lineHeight: "10px",
+    },
   },
 };
 
