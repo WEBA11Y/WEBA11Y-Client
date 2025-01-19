aaa3c9300a8054aa1c662f21847d956eb2f49073
juhee067 <66045537+juhee067@users.noreply.github.com>
2025-01-19 14:18:40 +0900

    style:rootLayout안에 header 생성

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { ThemeProvider } from "styled-components";
 import Router from "./routes/index";
 import theme from "./styles/theme";
 import GlobalStyle from "./styles/GlobalStyle";
+import RootLayout from "./layouts/RootLayout";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
+      <RootLayout />
       <GlobalStyle />
       <Router />
       main
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,25 @@
-import styled from "styled-components";
+import { styled } from "styled-components";
 
 import LogoImage from "../assets/logo/logo.svg";
+import Button from "./Button";
 
 export default function Header() {
   return (
     <HeaderContainer>
       <Logo src={LogoImage} alt='장애인 마크와 w가 합쳐진 서비스 로고' />
+      <Button size='medium' variant='fill'>
+        LOGIN
+      </Button>
     </HeaderContainer>
   );
 }
 
 const HeaderContainer = styled.header`
   width: 100%;
-  padding: 15px 0;
+  padding: 15px 20px;
   background-color: ${({ theme }) => theme.colors.white};
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Logo = styled.img`
