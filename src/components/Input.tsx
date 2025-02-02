087a8616a1c59497c72b4cc600165cd76f2578d7
juhee067 <66045537+juhee067@users.noreply.github.com>
2025-02-02 21:20:05 +0900

    chore:재사용 컴포넌트 수정

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,43 +1,82 @@
 import { styled } from "styled-components";
-import { FieldError, FieldValues, useForm } from "react-hook-form";
+import {
+  FieldError,
+  FieldValues,
+  Path,
+  UseFormRegister,
+} from "react-hook-form";
 
 import Typography from "./Typography";
+import Button from "./Button";
 
-type Props = {
-  name: string;
+type Props<T extends FieldValues> = {
+  name: Path<T>;
   label: string;
   type: string;
   placeholder?: string;
-  error?: FieldError;
+  error?: FieldError | string;
+  register: UseFormRegister<T>;
+  checkDuplicate?: () => void;
 };
 
-export default function Input({ name, type, label, placeholder }: Props) {
-  const { register } = useForm<FieldValues>();
+export default function Input<T extends FieldValues>({
+  name,
+  type,
+  label,
+  placeholder,
+  register,
+  error,
+  checkDuplicate,
+}: Props<T>) {
   return (
-    <InputContainer>
-      <label htmlFor={label}>
+    <InputContainer hasButton={!!checkDuplicate}>
+      <label htmlFor={name}>
         <Typography variant='caption' size='sm'>
           {label}
         </Typography>
       </label>
-
-      <StyledInput
-        type={type}
-        placeholder={placeholder}
-        {...register(name, { required: true })}
-      />
+      <div className='input-with-button'>
+        <StyledInput
+          id={name}
+          type={type}
+          placeholder={placeholder}
+          {...register(name, { required: "필수 입력 항목입니다." })}
+        />
+        {checkDuplicate && (
+          <Button
+            type='button'
+            variant='outline'
+            size='small'
+            onClick={checkDuplicate}
+          >
+            중복 확인
+          </Button>
+        )}
+      </div>
+      {error && (
+        <ErrorText>
+          {typeof error === "string" ? error : error.message}
+        </ErrorText>
+      )}
     </InputContainer>
   );
 }
 
-const InputContainer = styled.div`
+const InputContainer = styled.div<{ hasButton: boolean }>`
+  width: 100%;
   display: flex;
   flex-direction: column;
   margin-bottom: 20px;
+
+  .input-with-button {
+    display: flex;
+    align-items: center;
+    gap: ${({ hasButton }) => (hasButton ? "10px" : "0")};
+  }
 `;
 
 const StyledInput = styled.input`
-  margin-top: 5px;
+  flex: 1;
   padding: 12px;
   border: 1px solid ${({ theme }) => theme.colors.neutral[500]};
   border-radius: 8px;
@@ -49,8 +88,8 @@ const StyledInput = styled.input`
   }
 `;
 
-// const ErrorText = styled.span`
-//   margin-top: 5px;
-//   font-size: 12px;
-//   color: ${({ theme }) => theme.colors.primary[500]};
-// `;
+const ErrorText = styled.span`
+  margin-top: 5px;
+  font-size: 12px;
+  color: ${({ theme }) => theme.colors.primary[500]};
+`;
