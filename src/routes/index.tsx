877d01ce151c90d7b1d12712b7095cb747b44972
juhee067 <66045537+juhee067@users.noreply.github.com>
2025-03-25 12:58:12 +0900

    refactor:RootLayout 구조 변경

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,90 +1,82 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import RootLayout from "../layouts/RootLayout";
+import MainLayout from "../layouts/MainLayout";
+import AuthLayout from "../layouts/AuthLayout";
+import ContentLayout from "../layouts/ContentLayout";
+import MainPage from "../pages/MainPage";
 import SignInPage from "../pages/SignInPage";
 import SignUpPage from "../pages/SignUpPage";
-import AuthLayout from "../layouts/AuthLayout";
-import MainLayout from "../layouts/MainLayout";
-import RootLayout from "../layouts/RootLayout";
-import MainPage from "../pages/MainPage";
-import ContentLayout from "../layouts/ContentLayout";
 import HistoryPage from "../pages/HistoryPage";
 import DetailHistoryPage from "../pages/DetailHistoryPage";
 import DashboardPage from "../pages/DashboardPage";
-import { PATH } from "../constants/path";
 import ProtectedRoute from "../components/ProtectedRoute";
+import { PATH } from "../constants/path";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (
-      <RootLayout>
-        <MainLayout />
-      </RootLayout>
-    ),
-
+    element: <RootLayout />,
     children: [
       {
         path: "",
-        element: <MainPage />,
-      },
-    ],
-  },
-  {
-    path: "/auth",
-    element: (
-      <RootLayout>
-        <AuthLayout />
-      </RootLayout>
-    ),
-    children: [
-      {
-        path: "signin",
-        element: <SignInPage />,
+        element: <MainLayout />,
+        children: [
+          {
+            path: "",
+            element: <MainPage />,
+          },
+        ],
       },
       {
-        path: "signup",
-        element: <SignUpPage />,
-      },
-    ],
-  },
-  {
-    path: "/history",
-    element: (
-      <RootLayout>
-        <ContentLayout />
-      </RootLayout>
-    ),
-    children: [
-      {
-        path: "",
-        element: (
-          <ProtectedRoute allowedRoles={["user", "admin"]}>
-            <HistoryPage />
-          </ProtectedRoute>
-        ),
+        path: "auth",
+        element: <AuthLayout />,
+        children: [
+          {
+            path: "signin",
+            element: <SignInPage />,
+          },
+          {
+            path: "signup",
+            element: <SignUpPage />,
+          },
+        ],
       },
       {
-        path: ":history_id",
-        element: <DetailHistoryPage />,
+        path: "history",
+        element: <ContentLayout />,
+        children: [
+          {
+            path: "",
+            element: (
+              <ProtectedRoute allowedRoles={["user", "admin"]}>
+                <HistoryPage />
+              </ProtectedRoute>
+            ),
+          },
+          {
+            path: ":history_id",
+            element: (
+              <ProtectedRoute allowedRoles={["user", "admin"]}>
+                <DetailHistoryPage />
+              </ProtectedRoute>
+            ),
+          },
+        ],
       },
-    ],
-  },
-  {
-    path: PATH.DASHBOARD,
-    element: (
-      <RootLayout>
-        <ContentLayout />
-      </RootLayout>
-    ),
-    children: [
       {
-        path: "",
-        element: <DashboardPage />,
+        path: PATH.DASHBOARD,
+        element: <ContentLayout />,
+        children: [
+          {
+            path: "",
+            element: <DashboardPage />,
+          },
+        ],
       },
     ],
   },
 ]);
-
 function Router() {
   return <RouterProvider router={router} />;
 }
