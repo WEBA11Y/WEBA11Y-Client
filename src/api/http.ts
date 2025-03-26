eb20eda8affc4c5284c895d92ec998c19d663632
juhee067 <66045537+juhee067@users.noreply.github.com>
2025-03-26 19:02:10 +0900

    chore:리프레쉬 토큰 재발급 안되는 문제 해결

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+import { PATH } from "../constants/path";
+
 const httpClient = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   timeout: 1000,
-  withCredentials: true,
+  withCredentials: true, // 쿠키 기반 인증 시 필요함
   headers: { "Content-Type": "application/json" },
 });
 
@@ -21,21 +23,35 @@ httpClient.interceptors.request.use(
 );
 
 httpClient.interceptors.response.use(
-  (res) => res,
-  (error) => {
+  (res) => res, // 응답 성공적인 경우 그대로 반환
+  async (error) => {
     const originalReq = error.config;
+    // accessToken 만료로 401 응답 / 그리고 재시도한 적이 없을 경우
     if (error.response?.status === 401 && !originalReq._retry) {
-      originalReq._retry = true;
-
+      originalReq._retry = true; // 중복 재시도 막기 위함
       try {
-        axios.post(
-          `${import.meta.env.VITE_BASE_URL}/api/v1/reissuing-token`,
-          {},
-          { withCredentials: true }
+        // 토큰 재발급 요청
+        const res = await axios.get(
+          `${import.meta.env.VITE_BASE_URL}api/v1/reissuing-token`,
+          {
+            withCredentials: true,
+          }
         );
-        return httpClient(originalReq);
-      } catch (error) {
-        return Promise.reject(error);
+
+        const newAccessToken = res.data;
+
+        if (newAccessToken) {
+          localStorage.setItem("accessToken", newAccessToken);
+          // 원래 요청에 새 토큰을 넣어 다시 요청
+          originalReq.headers.Authorization = `Bearer ${newAccessToken}`;
+          return httpClient(originalReq);
+        } else {
+          throw new Error("재발급된 accessToken이 없습니다.");
+        }
+      } catch (reissueError) {
+        localStorage.removeItem("accessToken");
+        window.location.href = PATH.HOME;
+        return Promise.reject(reissueError);
       }
     }
     return Promise.reject(error);
