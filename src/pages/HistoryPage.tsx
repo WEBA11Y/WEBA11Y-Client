63fc28a68385bbaa8611e88111328995854047f6
juhee067 <66045537+juhee067@users.noreply.github.com>
2025-03-26 17:26:10 +0900

    chore:toast util 분리 #10

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -1,11 +1,11 @@
 import { styled } from "styled-components";
 import { useState } from "react";
-import { toast } from "react-toastify";
 
 import { Header, HistoryList, SearchFilterBar } from "../features/History";
 import EmptyHistory from "../features/History/components/EmptyHistory";
 import { useRoleAuth } from "../hooks/useRoleAuth";
 import { useUrls } from "../features/History/hooks/useUrls";
+import { showErrorToast } from "../features/Signup/utils/toast";
 
 export default function HistoryPage() {
   useRoleAuth();
@@ -24,18 +24,9 @@ export default function HistoryPage() {
   const toggleDeleteMode = (type: string) => {
     if (type === "cancel" || type === "delete") {
       setIsDeleteMode((prev) => !prev);
+      setCheckedItems([]);
     } else {
-      checkedItems.length === 0 &&
-        toast.error("URL이 선택되지 않았습니다", {
-          position: "bottom-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: false,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-        });
+      checkedItems.length === 0 && showErrorToast("URL이 선택되지 않았습니다");
 
       if (!checkedItems.length) return;
       // 확인 버튼을 눌렀을 때, 선택한 아이템이 있으면, 선택한 아이템 총 갯수 n개 삭제하시겠습니까? 모달 창 띄움
